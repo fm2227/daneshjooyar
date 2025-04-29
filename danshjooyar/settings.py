58ee0878094e641764bc4c3c6e7eebe0c67120c0
Farshad Mozafari <farshadmozafari.64@gmail.com>
2025-04-29 18:13:20 +0330

    migrate shop models

--- danshjooyar/settings.py
+++ danshjooyar/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'danshjooyar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'shop'/'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
