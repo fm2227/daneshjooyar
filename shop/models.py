a6dab96d46a978571631ef957c7e13b0288a1b3a
Farshad Mozafari <farshadmozafari.64@gmail.com>
2025-05-06 14:37:05 +0330

    Implement get deleted records

--- shop/models.py
+++ shop/models.py
@@ -2,45 +2,63 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Category(models.Model):
+class BaseModelManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted = False)
+
+
+class BaseModel(models.Model):
+    deleted = models.BooleanField(default=False, editable=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = BaseModelManager()
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
     title = models.CharField(max_length=100)
+
     def __str__(self):
         return self.title
 
-class Product(models.Model):
+
+class Product(BaseModel):
     title = models.CharField(max_length=100)
     content = models.TextField()
     price = models.IntegerField()
     image = models.ImageField()
     quantity = models.PositiveIntegerField()
     status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+
     def __str__(self):
         return self.title
-class Cart(models.Model):
+
+
+class Cart(BaseModel):
     quantity = models.PositiveIntegerField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
-class Order(models.Model):
-    total_price = models.DecimalField(decimal_places=2,max_digits=10)
+class Order(BaseModel):
+    total_price = models.DecimalField(decimal_places=2, max_digits=10)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.BooleanField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class OrderProduct(models.Model):
+class OrderProduct(BaseModel):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL,default=None,null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(decimal_places=2,max_digits=10)
-    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
 
 
 class PaymentLog(models.Model):
-    amount = models.DecimalField(decimal_places=2,max_digits=10)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
     created_at = models.DateTimeField(auto_now_add=True)
     user_id = models.PositiveIntegerField()
     order_id = models.PositiveIntegerField()
