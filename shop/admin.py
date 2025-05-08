dac89935ce5bb1d01afc7bfada41e3986d1c12bf
Farshad Mozafari <farshadmozafari.64@gmail.com>
2025-05-08 12:25:18 +0330

    Add accounts app

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from . import models
-admin.site.register(models.Cart)
-admin.site.register(models.Order)
-admin.site.register(models.OrderProduct)
-admin.site.register(models.Product)
 
 
 @admin.register(models.Category)
@@ -14,3 +10,42 @@ class CategoryAdmin(admin.ModelAdmin):
     def delete_queryset(self, request, queryset):
         for category in queryset:
             category.delete()
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'quantity', 'status']
+    search_fields = ['title']
+    list_filter = ['status']
+
+    def delete_queryset(self, request, queryset):
+        for product in queryset:
+            product.delete()
+
+
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'user', 'quantity']
+
+    def delete_queryset(self, request, queryset):
+        for cart in queryset:
+            cart.delete()
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'total_price', 'status']
+    list_filter = ['status']
+
+    def delete_queryset(self, request, queryset):
+        for order in queryset:
+            order.delete()
+
+
+@admin.register(models.OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id', 'product', 'quantity', 'price']
+
+    def delete_queryset(self, request, queryset):
+        for orderproduct in queryset:
+            orderproduct.delete()
